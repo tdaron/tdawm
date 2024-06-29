afd47b8ec534e450c5c0e0b273c69a5cc5ffbbc5
Théo Daron <theo@daron.be>
2024-06-29 10:06:17 +0200

    starting wm before startup applications. To avoid them to open windows before we get ran

--- src/main.rs
+++ src/main.rs
@@ -29,14 +29,14 @@ fn main() {
 fn run() -> Result<(), Box<dyn Error>> {
     let display_name = std::env::var("DISPLAY")?;
     let user_config: config::Config = config::load_config()?;
+    let adapter = x11::X11Adapter::new(&display_name)?;
+    let mut wm = TDAWm::new(adapter)?;
     info!("running startup");
     for cmd in user_config.startup.iter() {
         info!("executing {}", cmd);
         let mut command = shell(cmd);
         command.execute()?;
     }
-    let adapter = x11::X11Adapter::new(&display_name)?;
-    let mut wm = TDAWm::new(adapter)?;
     // if let Ok(_) = env::var("XEPHYR") {
     //     // wm.set_modifier_to_control();
     // }
